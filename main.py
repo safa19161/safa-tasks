6113f08b3a12a4cf50e04f5f3314398b86fd5007
safa19161 <safaalam19161@gmail.com>
2026-03-12 21:15:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,78 @@
 # 3. Update the SMTP ADDRESS to match your email provider.
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
 import smtplib
-import os
+import pandas
+import datetime as dt
+import random
+now =  dt.datetime.now()
+email= "eerieboom@gmail.com"
+password = "zcqyaoqybzizdpto"
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+read = pandas.read_csv("birthdays.csv")
+month = read["month"].tolist()
+day = read["day"].tolist()
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+
+print(month,day)
+
+if now.month in month and now.day in day:
+
+    choose = random.choice(["letter_1.txt", "letter_2.txt", "letter_3.txt"])
+    with open(choose, 'r') as msg:
+        content = msg.read()
+        name1 = read[(read.day == now.day) & (read.month == now.month)]
+        name2 = name1.name.item()
+        # one error left to resolve if two name1 found it will give again value error.
+
+        letter = content.replace("[NAME]", name2)
+
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+
+
+        email1 = read[(read.day == now.day) & (read.month == now.month)]
+        email2 = email1.email.item()
+
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(user=email, password=password)
+
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+            from_addr=email,
+            to_addrs=f"{email2}",
+
+            msg=f"subject:Happy Birthday\n\n{letter}")
+else:
+    print("no birthday")
+
+# from datetime import datetime
+# import pandas
+# import random
+# import smtplib
+# import os
+
+# # import os and use it to get the Github repository secrets
+# MY_EMAIL = os.environ.get("MY_EMAIL")
+# MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+# today = datetime.now()
+# today_tuple = (today.month, today.day)
+
+# data = pandas.read_csv("birthdays.csv")
+# birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+# if today_tuple in birthdays_dict:
+#     birthday_person = birthdays_dict[today_tuple]
+#     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+#     with open(file_path) as letter_file:
+#         contents = letter_file.read()
+#         contents = contents.replace("[NAME]", birthday_person["name"])
+
+#     with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+#         connection.starttls()
+#         connection.login(MY_EMAIL, MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs=birthday_person["email"],
+#             msg=f"Subject:Happy Birthday!\n\n{contents}"
+#         )
